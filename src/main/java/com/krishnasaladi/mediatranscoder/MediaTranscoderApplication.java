b47728444eb0443002ebdc09b41a02938cfe9420
krishna <skvamshi@gmail.com>
2024-04-22 22:12:06 +0530

    init commit

---
+++ src/main/java/com/krishnasaladi/mediatranscoder/MediaTranscoderApplication.java
@@ -0,0 +1,13 @@
+package com.krishnasaladi.mediatranscoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTranscoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTranscoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishnasaladi/mediatranscoder/MediaTranscoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishnasaladi.mediatranscoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaTranscoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
